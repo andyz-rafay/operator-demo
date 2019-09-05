5be5b4fe5e2e61af9a84d6940666e37ccc9fa188
Andy Zhou <azhou@rafay.co>
2019-09-05 01:40:21 -0700

    Fix setup-core.sh

--- noc/setup-core.sh
+++ noc/setup-core.sh
@@ -2,4 +2,4 @@ TB=andy-testbed1
 CORE=$TB-core-dev-node-0
 
 # run setup script
-ssh $CORE "git clone git@github.com:andyz-rafay/operator-demo.git && operator-demo/core/setup.sh"
+ssh $CORE "git clone https://github.com/andyz-rafay/operator-demo.git && operator-demo/core/setup-git.sh"
