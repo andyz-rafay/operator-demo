c862b6791a3f1c41dceebfd89f84f49b41c8a524
Andy Zhou <azhou@rafay.co>
2019-09-05 02:01:16 -0700

    Remove core/setup-git.sh

--- core/setup-git.sh
+++
@@ -1,2 +0,0 @@
-git config --global user.name "Andy Zhou"
-git config --global user.email "andy@rafay.co"
--- core/setup.sh
+++ core/setup.sh
@@ -1,1 +1,7 @@
-./setup-git.sh
+# setup git
+git config --global user.name "Andy Zhou"
+git config --global user.email "andy@rafay.co"
+
+# Install postgres too
+sudo apt install postgresql-client-common postgresql-client
+
