027599eb94f02c7c95a0cb8ad2ed6af7ec42b61c
Andy Zhou <azhou@rafay.co>
2019-09-05 14:46:50 -0700

    Add cluster scheduler installation

--- core/install-pg.sh
+++
@@ -1,7 +0,0 @@
-NS=rp
-kubectl create ns $NS
-kubectl -n $NS apply -f yaml/postgres-configmap.yaml
-kubectl -n $NS apply -f yaml/postgres-storage.yaml
-kubectl -n $NS apply -f yaml/postgres-deployment.yaml
-kubectl -n $NS apply -f yaml/postgres-service.yaml
-
---
+++ core/install.sh
@@ -0,0 +1,14 @@
+NS=rp
+kubectl create ns $NS
+
+kubectl create-n $NS secret docker-registry dev-registry-creds --docker-server=egistry.dev.rafay-edge.net:5000 --docker-username=rcloud_user --docker-password=rcloud_mcpep&17_user --docker-email=andy@rafay.co
+
+# postgres
+kubectl -n $NS apply -f yaml/postgres-configmap.yaml
+kubectl -n $NS apply -f yaml/postgres-storage.yaml
+kubectl -n $NS apply -f yaml/postgres-deployment.yaml
+kubectl -n $NS apply -f yaml/postgres-service.yaml
+
+# cluster scheduler
+kubectl -n $NS apply -f yaml/cluster-scheduler-deployment.yaml
+kubectl -n $NS apply -f yaml/cluster-scheduler-service.yaml
