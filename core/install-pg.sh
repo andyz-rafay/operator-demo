58bf53006b9d6fe4d4616947382f6f24e1145784
Andy Zhou <andy@rafay.co>
2019-09-05 17:33:49 +0000

    Fix install-pg.s script

--- core/install-pg.sh
+++ core/install-pg.sh
@@ -2,6 +2,6 @@ NS=rp
 kubectl create ns $NS
 kubectl -n $NS apply -f yaml/postgres-configmap.yaml
 kubectl -n $NS apply -f yaml/postgres-storage.yaml
-kubectl -n $NS apply -f yaml/postgres-depolyment.yaml
+kubectl -n $NS apply -f yaml/postgres-deployment.yaml
 kubectl -n $NS apply -f yaml/postgres-service.yaml
 
