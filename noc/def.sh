0432556fd14cea298f1008443287457cb994eefc
Andy Zhou <azhou@rafay.co>
2019-09-05 10:27:08 -0700

    Add ssh-core.sh

--- noc/def.sh
+++ noc/def.sh
@@ -1,4 +1,5 @@
-. ./def.sh
+TB=andy-testbed1
+CORE=$TB-core-dev-node-0
 
 # run setup script
 ssh $CORE "git clone https://github.com/andyz-rafay/operator-demo.git && operator-demo/core/setup.sh"
---
+++ noc/ssh-core.sh
@@ -0,0 +1,3 @@
+. ./def.sh
+
+ssh $CORE
