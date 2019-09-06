22b5b249346be1c2028c58da2c3504605bf23f1e
Andy Zhou <andy@rafay.co>
2019-09-06 06:00:53 +0000

    Fix yaml files

--- core/delete-pg.sh
+++ core/delete-pg.sh
@@ -1,7 +1,7 @@
-NS ?= rp
+NS=rp
 kubectl delete service postgres
-kubectl delete -n $NS deployment postgres
-kubectl delete -n $NS configmap postgres-config
+kubectl delete -n $NS deployment.app/postgres
+kubectl delete -n $NS configmap/postgres-config
 kubectl delete persistentvolumeclaim postgres-pv-claim
 kubectl delete persistentvolume postgres-pv-volume
 
